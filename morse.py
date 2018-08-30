6d70f9616a6146277d866b4e8d1a7fb276adf678
Matt Williams <matt@milliams.com>
2018-08-30 13:43:35 +0100

    Add doctest to morse code

--- morse.py
+++ morse.py
@@ -8,9 +8,19 @@ letter_to_morse = {'a':'.-', 'b':'-...', 'c':'-.-.', 'd':'-..', 'e':'.', 'f':'..
 
 
 def encode(message: str) -> str:
+    """
+    Given a string of charaters, return the morse message as a string
+    """
     return ' '.join(letter_to_morse[l] for l in message)
 
 
 def decode(morse_message: str) -> str:
+    """
+    Given a morse message, return the english message
+
+    Examples:
+        >>> decode("... --- ...")
+        'sos'
+    """
     morse_to_letter = {v: k for k, v in letter_to_morse.items()}
     return ''.join(morse_to_letter[c] for c in morse_message.split())
